bb530399ebbb332d745f7a435661f70f90fd499f
Davide Greco <greco.gdavide@gmail.com>
2022-02-25 21:15:11 +0100

    added placeholders, written readme

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # coderunner.nvim
+---
 Run code in neovim
 
-Note that this is a WIP
+# Requirements
+---
+This plugin requires neovim >= 0.6
+
+# Setup
+---
+to set up this plugin, in a lua file require it and give it the commands:
+```lua
+local cr = require("coderunner")
+cr.commands = {
+    sh = "${FP}",
+    py = "python ${FP}"
+}
+```
+Available placeholders are:
+- `${FP}` for the full path
+- `${NE}` for the file name
+- `${N}`  for the file name without extension
+
+# Features
+---
+Run current file in a neovim terminal.  
+The available functions are:  
+- `run_current`: creates a new buffer
+- `run_current_split`: creates a new split
+- `run_current_vsplit`: creates a new vertical split
+
+And the commands to do the same are:
+- `RunCurrent`
+- `RunCurrentSplit`
+- `RunCurrentVSplit`
--- lua/coderunner.lua
+++ lua/coderunner.lua
@@ -2,13 +2,17 @@ local M = {}
 local api = vim.api
 M.commands = {}
 
-local function run(ext)
+local function run(ext, command)
     -- replace placeholders
     local c = M.commands[ext]
-    c = c:gsub("${NE}", vim.fn.expand("%")):gsub("${N}", vim.fn.expand("%:r"))
+    if c then
+        c = c:gsub("${NE}", vim.fn.expand("%")):gsub("${N}", vim.fn.expand("%:p:r")):gsub("${FP}", vim.fn.expand("%:p"))
+    else
+    c = "echo 'command for \"" .. ext .. "\" not found'"
+    end
 
     -- open terminal with the right command
-    api.nvim_command("term " .. c)
+    api.nvim_command(command .. c)
 
     -- go in insert mode
     api.nvim_input("A")
@@ -16,11 +20,22 @@ local function run(ext)
     vim.keymap.set({"n", "i", "t"}, "<Esc>", "<C-\\><C-n>" , {buffer = 0})
 end
 
+-- create functions and commands
 M.run_current = function()
-   run(vim.fn.expand("%:e"))
+   run(vim.fn.expand("%:e"), "term ")
 end
 
-api.nvim_add_user_command("RunCurrentFile", function() return M.run_current() end, {})
+M.run_current_split = function()
+   run(vim.fn.expand("%:e"), "split | term ")
+end
+
+M.run_current_vsplit = function()
+   run(vim.fn.expand("%:e"), "vsplit | term ")
+end
+
+api.nvim_add_user_command("RunCurrent", function() return M.run_current() end, {})
+api.nvim_add_user_command("RunCurrentSplit", function() return M.run_current_split() end, {})
+api.nvim_add_user_command("RunCurrentVSplit", function() return M.run_current_vsplit() end, {})
 
 
 return M
